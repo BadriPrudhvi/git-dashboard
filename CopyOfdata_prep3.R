feb53e905962b45b0bc7845cbf8ad8b3285fc467
loiyumba <loiyumba@gmail.com>
2016-06-08 15:44:37 +0530

    Clean code

---
+++ CopyOfdata_prep3.R
@@ -0,0 +1,11 @@
+setwd("D:\\2016\\Data_Science\\Data\\pitambara")
+
+sample <- read.csv("new_sample3.csv", stringsAsFactors = FALSE)
+str(sample)
+names(sample) <- c("Class", "Name", "Examination", "Date", "Subject",
+                   "Max_Mark", "Pass_Mark", "Obtain_Mark", "Subject_Percentage",
+                   "Class_Average", "Exam_Percentage")
+
+write.csv(sample, "new_sample4.csv", row.names = FALSE)
+
+
